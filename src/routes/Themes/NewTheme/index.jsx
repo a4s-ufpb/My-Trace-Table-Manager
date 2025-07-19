74b950074545b80ff72380ad4b416b9d22742663
Anna Gabriela <agmsouza23@gmail.com>
2025-07-19 15:15:28 -0300

    Feat: adicionando ajuda para as funcionalidades da página de temas

--- src/routes/Themes/NewTheme/index.jsx
+++ src/routes/Themes/NewTheme/index.jsx
@@ -4,6 +4,8 @@ import useThemeCollection from "../../../hooks/useThemeCollection";
 import ListItems from "../../../components/ListItems";
 import MessagePopUp from "../../../components/MessagePopUp";
 import PageChanging from "../../../components/PageChanging";
+import { BsQuestionCircleFill } from "react-icons/bs";
+import HelpPopUp from "../../../components/HelpPopUp";
 
 export default function NewTheme() {
     const [theme, setTheme] = useState("");
@@ -12,6 +14,7 @@ export default function NewTheme() {
     const [showMessagePopUp, setShowMessagePopUp] = useState(false);
     const [popUpMessage, setPopUpMessage] = useState(""); 
     const { themes, addTheme, editTheme, removeTheme, currentPage, totalPages, changePage } = useThemeCollection();
+    const [openHelpPopUp, setOpenHelpPopUp] = useState(false);
     const navigate = useNavigate();
 
     function handleSubmit(event) {
@@ -46,10 +49,17 @@ export default function NewTheme() {
         setOnEdit(false);
     }
 
+    const showHelpPopUp = () => {
+        setOpenHelpPopUp(true);
+    };
+
     return (
         <div className="background">
             <div className="form-bg">
-                {onEdit ? <h2>Editar tema</h2> : <h2>Cadastrar novo tema</h2>}
+                <div className="content-with-help">
+                    {onEdit ? <h2>Editar tema</h2> : <h2>Cadastrar novo tema</h2>}
+                    <BsQuestionCircleFill className="icon-question" onClick={showHelpPopUp} />
+                </div>
                 <form onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="theme">Informe o tema</label>
@@ -93,14 +103,18 @@ export default function NewTheme() {
                     totalPages={totalPages}    
                 />
             </div>
-            {
-                showMessagePopUp && (
-                    <MessagePopUp
-                        message={popUpMessage} 
-                        showPopUp={setShowMessagePopUp}
-                    />
-                )
-            }
+            {openHelpPopUp && (
+                <HelpPopUp
+                    text='Para cadastrar um novo tema, o professor deve informar o nome no campo indicado e clicar no botão "Cadastrar". Caso deseje editar um tema já existente, basta clicar no ícone de lápis ao lado do tema, fazer a alteração desejada e, em seguida, salvar ou cancelar a edição. Para remover um tema, é só clicar no ícone de lixeira correspondente. O número exibido à esquerda de cada tema representa o código identificador do tema. A navegação entre os temas cadastrados pode ser feita pelos botões "Anterior" e "Próximo", localizados na parte inferior da tela.'
+                    onClose={() => setOpenHelpPopUp(false)}
+                />
+            )}
+            {showMessagePopUp && (
+                <MessagePopUp
+                    message={popUpMessage} 
+                    showPopUp={setShowMessagePopUp}
+                />
+            )}
         </div >
     )
 }
