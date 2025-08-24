8589a52e4ccae1441278a50a0f10606795a2482b
Anna Gabriela <agmsouza23@gmail.com>
2025-08-24 18:00:32 -0300

    Feat: mostrando nome do usuário no Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,12 +10,23 @@ import { BiMenu } from "react-icons/bi";
 export default function Header() {
     const navigate = useNavigate();
 
+    const [userName, setUserName] = useState("");
+
     const [menu, setMenu] = useState(false);
     const [openLogoutPopUp, setOpenLogoutPopUp] = useState(false);
     const [openPersonOptions, setOpenPersonOptions] = useState(false);
 
     const personOptionsRef = useRef(null);
     const personIconRef = useRef(null);
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (user && user.name) {
+            const nomeCompleto = user.name;
+            const primeiroNome = nomeCompleto.split(' ')[0];
+            setUserName(primeiroNome);
+        }
+    }, []);
 
     useEffect(() => {
         function handleClickOutside(ev) {
@@ -46,12 +57,14 @@ export default function Header() {
                 className={styles.rightOptionsContainer}
                 data-testid="person-icon-container"
             >
-                <BsPersonFill
-                    className={styles.btnPerson}
-                    onClick={() => {
-                        setOpenPersonOptions(prev => !prev);
-                    }}
-                />
+                <div className={styles.personIconAndName} onClick={() => {
+                    setOpenPersonOptions(prev => !prev);
+                }}>
+                    {userName && <span className={styles.welcomeMessage}>{userName}</span>}
+                    <BsPersonFill
+                        className={styles.btnPerson}
+                    />
+                </div>
                 <img
                     src={logoA4S}
                     alt="logo-a4s"
@@ -64,6 +77,12 @@ export default function Header() {
                     ref={personOptionsRef}
                     className={`${styles.personOptions} ${openPersonOptions ? styles.active : ''}`}
                 >
+                    <>
+                        <div className={styles.personInfo}>
+                            <span>Olá, {userName}</span>
+                        </div>
+                        <hr className={styles.separator} />
+                    </>
                     <p className={styles.personOption} onClick={() => {
                         setOpenPersonOptions(false);
                         navigate("profile");
@@ -79,6 +98,7 @@ export default function Header() {
                 <AttentionPopUp
                     text="Tem certeza que deseja sair?"
                     confirmAction={() => {
+                        localStorage.removeItem("user");
                         localStorage.removeItem("token")
                         localStorage.removeItem("tokenExpiration")
                         localStorage.removeItem("userId")
